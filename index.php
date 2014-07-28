8cc713ede8dd3d4c7232dc94ea723ecc397febef
Klee <kleetang@gmail.com>
2014-07-28 16:06:00 -0400

    Change index.php

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="./img/icon.png">
+
+    <title>Saint Peter's Acadmy</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="./css/main.css" rel="stylesheet">
+    <!-- Common styles -->
+    <link href="./css/style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="./assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container">
+
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="./img/logo.png"/></a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="tasc.php">TASC</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Programs<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+                <li><a href="course.php">Course Guide</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+
+      <div class="jumbotron">
+        <h2>Saint Peter's Academy</h2>
+        <p class="lead">Saint Peter's Academy is a college-level preparatory school which was registered<br> in New York City.</p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-12">
+          <p><strong>Saint Peter's Academy</strong> mainly provides educational services, including <strong>Test Assessing Secondary Completion (TASC)</strong> diploma courses, college-level <strong>Advanced Placement (AP)</strong> curriculum, and <strong>1+3 University Pathway Program (UPP).</strong> It also provides <strong>SAT</strong> and <strong>ESL</strong> courses.</p>
+        </div>
+      </div>
+
+      <div class="row bottom">
+        <div class="col-md-8 sitemap">
+          <div row>
+            <div class="col-md-2">
+              <a href="index.php">Home</a>
+            </div>
+            <div class="col-md-2">
+              <a href="tasc.php">TASC</a>
+            </div>
+            <div class="col-md-2">
+              <a href="program.php">programs</a>
+            </div>
+            <div class="col-md-2">
+              <a href="course.php">Course Guide</a>
+            </div>
+            <div class="col-md-2">
+              <a href="about.php">About Us</a>
+            </div>
+            <div class="col-md-2">
+              <a href="contact.php">Contact Us</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 address">
+          <h4>Columbia International Academy</h4>
+          <p>Email: <a href="mailto:cianyorg@gmail.com">cianyorg@gmail.com</a><br>
+          Phone: 1-212-825-0437<br>
+          Address: 17 Battery Place, Suite 300, New York, NY 10004</p>
+        </div>
+      </div>
+      <div class="footer">
+        <p>© American Education Center 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="./bootstrap/dist/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
